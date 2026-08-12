0bcfe0d0e61bac325e52014c9b04d6c93617a0c7
zyasminah <zahmouniyasmina@gmail.com>
2026-08-12 12:55:19 +0100

    Add finished bill-splitter script

---
+++ bill-splitter/main.py
@@ -0,0 +1,21 @@
+running_total = 0
+
+num_of_friends = 4
+
+appetizers = 37.89
+main_courses = 57.34
+desserts = 39.39
+drinks = 64.21
+
+running_total += appetizers + main_courses + desserts + drinks
+print('Total bill so far:', running_total)
+
+tip = running_total * 0.25
+print('Tip amount:', tip)
+
+running_total += tip
+print('Total with tip:', running_total)
+
+final_bill = running_total / num_of_friends
+print('Bill per person:', final_bill)
+
